a1510cf6463d11d286c462508b584b142f5d61eb
EmanuelFuneS <emanuel.ignacio.funes@gmail.com>
2025-07-20 22:43:26 -0300

    feat: added library and extend language switched with flags

--- components/language-switched/index.tsx
+++ components/language-switched/index.tsx
@@ -4,6 +4,10 @@ import { Select, SelectItem } from '@heroui/select'
 import { useI18n } from '@/app/i18n/context'
 import { locales } from '@/app/i18n'
 import { useRouter, usePathname } from 'next/navigation'
+import { US, ES } from 'country-flag-icons/react/3x2'
+import Typography from '../ui/typography';
+
+
 const LanguageSwitched = () => {
     const router = useRouter()
     const pathname = usePathname()
@@ -17,14 +21,24 @@ const LanguageSwitched = () => {
         router.push(newPath)
     }
 
-    console.log(locale)
     return (
         <div className='w-20'>
-            <Select defaultSelectedKeys={locale}
+            <Select defaultSelectedKeys={[locale]}
+                startContent={locale === "en" ? <US className='w-10' /> : <ES className='w-10' />}
                 color='default'
                 name="language" id="lang" onChange={(e) => changeLanguage(e.target.value)}>
-                <SelectItem key={'en'}>EN</SelectItem>
-                <SelectItem key={'es'}>ES</SelectItem>
+                <SelectItem key={'en'} >
+                    <div className='flex items-center justify-between w-full'>
+                        <US className='w-4 h-4' />
+                        <Typography as='span' variant='paragraph'>EN</Typography>
+                    </div>
+                </SelectItem>
+                <SelectItem key={'es'}>
+                    <div className='flex items-center justify-between w-full'>
+                        <ES className='w-4 h-4' />
+                        <Typography as='span' variant='paragraph'>ES</Typography>
+                    </div>
+                </SelectItem>
             </Select >
         </div >
     )
