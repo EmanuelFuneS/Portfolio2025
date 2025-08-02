45d734767323464d565e48d0ce6a46fbf53af835
EmanuelFuneS <emanuel.ignacio.funes@gmail.com>
2025-08-01 23:36:38 -0300

    fix: added tootip, and fix description pojects

--- app/[locale]/(main)/projects/page.tsx
+++ app/[locale]/(main)/projects/page.tsx
@@ -4,17 +4,26 @@ const screenshotsUrls = [
   {
     name: 'Simple Design Hotel',
     url: 'https://simple-design-hotel-web.vercel.app/',
+    description:
+      'A simple hotel booking website with a modern design. Techs used are Next.js, React, Tailwind CSS, and TypeScript.',
   },
   {
     name: 'Todo API Documentation',
     url: 'https://todo-api-production-877c.up.railway.app/docs',
+    description:
+      'API documentation for a simple todo application. Techs used are Python, FastAPI, MongoDB, and Docker.',
   },
 ]
 const Page = () => {
   return (
     <div className='flex flex-wrap items-center justify-center w-auto px-10 m-10 '>
       {screenshotsUrls.map((project, index: number) => (
-        <CardProject key={index} url={project.url} name={project.name} />
+        <CardProject
+          key={index}
+          url={project.url}
+          name={project.name}
+          description={project.description}
+        />
       ))}
     </div>
   )
--- components/card-project/index.tsx
+++ components/card-project/index.tsx
@@ -3,44 +3,58 @@ import Link from 'next/link'
 import { Button } from '@heroui/button'
 import { Card, CardFooter } from '@heroui/card'
 import { Image } from '@heroui/image'
+import { Tooltip } from '@heroui/tooltip'
 
 interface CardProjectProps {
   url: string
   name: string
+  description?: string
 }
 
-const CardProject = ({ url, name }: CardProjectProps) => {
+const CardProject = ({ url, name, description }: CardProjectProps) => {
   const imageUrl = `https://image.thum.io/get/width/1200/crop/800/${url}`
   return (
-    <Card
-      isFooterBlurred
-      className='border-none m-4 w-[280px]'
-      radius='lg'
-      shadow='md'
+    <Tooltip
+      content={
+        <div className='px-1 py-2 max-w-[1400px] text-ellipsis'>
+          <div className='font-bold text-small'>{name}</div>
+          <div className='text-tiny '>{description}</div>
+        </div>
+      }
+      placement='top'
+      showArrow
+      classNames={{
+        base: ['before:bg-[#ededed] dark:bg-[#2A2A2A] rounded-full'],
+        content: [
+          'py-2 px-4 shadow-xl',
+          'bg-linear-to-br from-[#ededed] to-neutral-400',
+        ],
+      }}
     >
-      <Image
-        alt='Project screenshot'
-        className='object-fill'
-        height={230}
-        src={imageUrl}
-      />
-      <CardFooter className='justify-between dark:bg-[#2A2A2A] before:bg-white/10 border-white/20 border-1 overflow-hidden py-1 absolute before:rounded-xl rounded-large bottom-1 w-[calc(100%_-_8px)] shadow-small ml-1 z-10'>
-        <Button
-          className='w-full text-tiny bg-[#2A2A2A]/40 backdrop-blur-sm'
-          color='default'
-          radius='lg'
-          size='sm'
-          variant='flat'
-        >
-          <Link
-            href={url}
-            className='text-[#ededed] font-semibold drop-shadow-lg [text-shadow:_0_1px_2px_rgb(0_0_0_/_40%)]'
+      <Card isFooterBlurred className='m-4 border-none' radius='lg' shadow='md'>
+        <Image
+          alt='Project screenshot'
+          className='object-fill md:w-[280px] md:h-[230px]'
+          src={imageUrl}
+        />
+        <CardFooter className='justify-between dark:bg-[#2A2A2A] before:bg-white/10 border-white/20 border-1 overflow-hidden py-1 absolute before:rounded-xl rounded-large bottom-1 w-[calc(100%_-_8px)] shadow-small ml-1 z-10'>
+          <Button
+            className='w-full text-tiny bg-[#2A2A2A]/40 backdrop-blur-sm'
+            color='default'
+            radius='lg'
+            size='sm'
+            variant='flat'
           >
-            CLICK TO VISIT {name}
-          </Link>
-        </Button>
-      </CardFooter>
-    </Card>
+            <Link
+              href={url}
+              className='text-[#ededed] font-semibold drop-shadow-lg [text-shadow:_0_1px_2px_rgb(0_0_0_/_40%)]'
+            >
+              CLICK TO VISIT
+            </Link>
+          </Button>
+        </CardFooter>
+      </Card>
+    </Tooltip>
   )
 }
 
