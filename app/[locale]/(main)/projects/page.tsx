b7d0b9b037ae97452a10c4714a3f8c58ec8ccfde
EmanuelFuneS <emanuel.ignacio.funes@gmail.com>
2025-08-06 20:44:41 -0300

    added: proyects

--- app/[locale]/(main)/projects/page.tsx
+++ app/[locale]/(main)/projects/page.tsx
@@ -1,19 +1,56 @@
 'use client'
 
 import CardProject from '@/components/card-project'
+import swagger from '@/public/Swagger.png'
+import agile from '@/public/agileStrat.png'
+import github from '@/public/github.jpg'
+import lasVpn from '@/public/laslesvpn.png'
+import luxury from '@/public/luxury.png'
 
 const screenshotsUrls = [
   {
+    image: luxury,
     name: 'Simple Design Hotel',
     url: 'https://simple-design-hotel-web.vercel.app/',
     description:
       'A simple hotel booking website with a modern design. Techs used are Next.js, React, Tailwind CSS and TypeScript.',
   },
   {
+    image: swagger,
     name: 'Todo API Documentation',
     url: 'https://todo-api-production-877c.up.railway.app/docs',
     description:
       'API documentation for a simple todo application. Techs used are Python, FastAPI, PyTest, MongoDB.',
+  },
+  {
+    image: github,
+    name: 'E-Commerce ByteBazar',
+    url: 'https://github.com/EmanuelFuneS/e-commerce',
+    description:
+      'E-Commerce in progress. Techs used are Turbo Repo, NextJS, TS, Tailwind, Prisma, Postgres, Auth0, Cloudinary ',
+  },
+  {
+    image: github,
+    name: 'ClimApp',
+    url: 'https://github.com/EmanuelFuneS/react-native-apps/tree/main/apps/native',
+    description: 'Weather app, developed in Expo, ReactNative, weatherApi',
+  },
+  {
+    image: github,
+    name: 'TodoApp',
+    url: 'https://github.com/EmanuelFuneS/flutterApp',
+    description: 'Todo app, developed in flutter',
+  },
+  {
+    image: lasVpn,
+    name: 'LaslesVpn',
+    url: 'https://github.com/EmanuelFuneS/flutterApp',
+  },
+  {
+    image: agile,
+    name: 'AgileStrat',
+    url: 'https://agilestrat-frontend.vercel.app/',
+    description: 'Techs are used Vite, React, TS, css',
   },
 ]
 const Page = () => {
@@ -22,6 +59,7 @@ const Page = () => {
       {screenshotsUrls.map((project, index: number) => (
         <CardProject
           key={index}
+          image={project.image}
           url={project.url}
           name={project.name}
           description={project.description}
--- components/card-project/index.tsx
+++ components/card-project/index.tsx
@@ -2,6 +2,7 @@
 
 import { useState } from 'react'
 
+import { StaticImageData } from 'next/image'
 import Link from 'next/link'
 
 import { Button } from '@heroui/button'
@@ -12,24 +13,25 @@ interface CardProjectProps {
   url: string
   name: string
   description?: string
+  image: StaticImageData
 }
 
-const CardProject = ({ url, name, description }: CardProjectProps) => {
+const CardProject = ({ url, name, description, image }: CardProjectProps) => {
   const [isHovered, setIsHovered] = useState(false)
-  const imageUrl = `https://image.thum.io/get/width/1200/crop/800/${url}`
+
   return (
     <Card isFooterBlurred className='m-4 border-none' radius='lg' shadow='md'>
       <Image
         alt='Project screenshot'
-        className='object-fill md:w-[280px] md:h-[230px]'
-        src={imageUrl}
+        className='object-cover md:w-[280px] md:h-[230px]'
+        src={image.src}
       />
 
       <CardFooter
         onMouseEnter={() => setIsHovered(true)}
         onMouseLeave={() => setIsHovered(false)}
         className={`flex flex-col justify-start dark:bg-[#2A2A2A] before:bg-white/10 border-white/20 border-1 overflow-hidden py-2 absolute before:rounded-xl rounded-large bottom-1 w-[calc(100%_-_8px)] shadow-small ml-1 z-10 transition-all duration-700 ease-out ${
-          isHovered
+          isHovered && description?.length
             ? 'h-32' // Altura expandida
             : 'h-14' // Altura contraída (solo botón)
         }`}
@@ -48,7 +50,7 @@ const CardProject = ({ url, name, description }: CardProjectProps) => {
             CLICK TO VISIT
           </Link>
         </Button>
-        {isHovered && (
+        {isHovered && description && (
           <div className='px-1 py-2 text-ellipsis'>
             <div className='font-bold text-small'>{name}</div>
             <div className='text-tiny'>{description}</div>
