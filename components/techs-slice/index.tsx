7ff3b72796bcf6e5361d133d2d0b930895cf09e9
EmanuelFuneS <emanuel.ignacio.funes@gmail.com>
2026-03-27 16:08:22 -0300

    fix: css icon, build success

--- components/techs-slice/index.tsx
+++ components/techs-slice/index.tsx
@@ -1,5 +1,5 @@
 import {
-  SiCss3,
+  SiCss,
   SiDocker,
   SiFastapi,
   SiHtml5,
@@ -26,7 +26,7 @@ const TechsSlice = () => {
       <SiNextdotjs className='w-12 h-12 text-black transition-transform duration-300 dark:text-white hover:scale-110' />
       <SiReact className='w-12 h-12 text-[#61DAFB] hover:scale-110 transition-transform duration-300' />
       <SiHtml5 className='w-12 h-12 text-[#E34F26] hover:scale-110 transition-transform duration-300' />
-      <SiCss3 className='w-12 h-12 text-[#1572B6] hover:scale-110 transition-transform duration-300' />
+      <SiCss className='w-12 h-12 text-[#1572B6] hover:scale-110 transition-transform duration-300' />
       <SiPostgresql className='w-12 h-12 text-[#336791] hover:scale-110 transition-transform duration-300' />
       <SiPrisma className='w-12 h-12 text-[#2D3748] hover:scale-110 transition-transform duration-300' />
       <SiMysql className='w-12 h-12 text-[#4479A1] hover:scale-110 transition-transform duration-300' />
